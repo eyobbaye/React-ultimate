b87c3c56a003a6a6d4dd5bbcc33e74e4d9caad73
eyobbaye <eyob.bay@gmail.com>
2026-01-08 12:47:21 +0300

    fetch advice from API & log to console

--- first-app-advice/src/App.jsx
+++ first-app-advice/src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  async function getAdvice() {
+   const res= fetch("https://api.adviceslip.com/advice")
+   const data= (await res).json()
+   console.log(data)
+
+  }
 
   return (
     <>
       <div>
         <h1>Advice Slip</h1>
-        <button>Get Advice</button>
+        <button onClick={getAdvice}>Get Advice</button>
       </div>
       
     </>
