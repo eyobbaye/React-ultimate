27c5dfc8077b9986c35983d5d59390102c70a8e2
eyobbaye <eyob.bay@gmail.com>
2026-01-15 14:08:48 +0300

    fetch pizzaData

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import pizzaData from "./data.js";
+import "./App.css";
 
 function App() {
+  return (
+    <>
+      <div>
+        <h2>Pizza</h2>
+        <PizzaMenu />
+      </div>
+    </>
+  );
+}
 
+// Pizza Menu app component
+
+function PizzaMenu() {
   return (
     <>
       <div>
         <h2>Pizza Menu</h2>
+        <ul>
+          {pizzaData.map((pizza) => (
+            <li>
+              {pizza.name} - ${pizza.price}
+              <p>Ingredients: {pizza.ingredients}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
