a221bdc077a5f8bf492601058783afb4a62c3e34
eyobbaye <eyob.bay@gmail.com>
2026-01-15 15:02:52 +0300

    Create Pizza menu components

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -6,28 +6,56 @@ function App() {
   return (
     <>
       <div>
-        <h2>Pizza</h2>
-        <PizzaMenu />
+        <Header />
+        <Menu />
+        <Footer />
       </div>
     </>
   );
 }
 
 // Pizza Menu app component
+// Header
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+// Menu
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <PizzaMenu />
+    </div>
+  );
+}
+// Footer
+function Footer() {
+  return (
+    <footer>
+      <p>We're currently Open!</p>
+    </footer>
+  );
+}
 
 function PizzaMenu() {
   return (
     <>
       <div>
-        <h2>Pizza Menu</h2>
+        {/* <h2>Our Menu</h2> */}
         <ul>
           {pizzaData.map((pizza) => (
             <li>
-              {pizza.name} - ${pizza.price}
+              <img src= {pizza.photoName} alt={pizza.name} width ="100" />
+              <p>{pizza.name} - ${pizza.price}</p>
               <p>Ingredients: {pizza.ingredients}</p>
             </li>
           ))}
         </ul>
+        {/* <img src="pizzas/focaccia.jpg" alt="{pizza.name}" width={"100"} />
+        <h2>
+          {pizzaData[0].name} - ${pizzaData[0].price}
+        </h2>
+        <p>Ingredients: {pizzaData[0].ingredients}</p> */}
       </div>
     </>
   );
