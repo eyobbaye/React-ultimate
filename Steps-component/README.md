8b93de47410bf7b4f4b79fdd33db497ab9b4631f
eyobbaye <eyob.bay@gmail.com>
2026-01-23 17:34:08 +0300

    use state in react and finshed up the steps-component

--- Steps-component/README.md
+++ Steps-component/README.md
@@ -1,3 +1,7 @@
 # React + Vite
 
-This template provides a minimal way how to create Steps components in React JS.
+This template provides a minimal way how to create Steps components and how to use states in React JS.
+re-rendering conponents and re-renderign the UI of states.
+Seteps components,
+useState(),
+state variable.
--- Steps-component/src/App.jsx
+++ Steps-component/src/App.jsx
@@ -1,30 +1,53 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
+  "Debug code error  ",
 ];
 
 function App() {
-  const step =3;
+  const [step, setStep] = useState(1);
+  // const [currentStep, setCurrentStep]= useState(step)
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handlePrvious() {
+    step >= messages.length - 2 ? setStep(step - 1) : " ";
+  }
+  function handleNext() {
+    step <= messages.length - 1 ? setStep(step + 1) : "";
+  }
+  function handleIsOpen(){
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
-      <div className="steps">
-        <div className="numbers">
-          <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-          <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-          <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      <button className="close" onClick={handleIsOpen}>&times;</button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+            <div className={`${step >= 4 ? "active" : ""}`}>4</div>
+          </div>
+          <div className="message">
+            Step {step}: {messages[step - 1]}
+          </div>
+          <div className="buttons">
+            <button className="button" onClick={handlePrvious}>
+              <span>Previous</span>
+            </button>
+            <button className="button" onClick={handleNext}>
+              <span>Next</span>
+            </button>
+          </div>
         </div>
-        <div className="message">
-        Step {step}: {messages [step-1]}
-        </div>
-        <div className="buttons">
-        <button className="button"><span>Previous</span></button>
-        <button className="button"><span>Next</span></button>
-        </div>
-      </div>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
